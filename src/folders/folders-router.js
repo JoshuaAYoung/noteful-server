33864b26f5ffc0d7b59f0bdac62a1fc7434fd356
JoshuaAYoung <joshua@young.net>
2020-02-27 22:53:13 -0700

    backend almost done

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -17,11 +17,61 @@ foldersRouter
   .get((req, res, next) => {
     foldersService.getAllFolders(req.app.get('db'))
       .then(folders => {
-        console.log(folders)
         res.status(200).json(folders.map(serializeFolders))
       })
       .catch(next)
   })
+
+  .post((req, res, next) => {
+
+    const { name } = req.body;
+
+    if (!name) {
+      return res
+        .status(400).send('Folder name is required');
+    }
+
+    foldersService.insertFolder(req.app.get('db'), name)
+      .then(result => {
+        return res.status(201)
+          .location(`/api/folders/${result.id}`)
+          .json(result)
+      })
+      .catch(next)
+
+  })
+
+
+foldersRouter
+  .route('/:id')
+  .get((req, res, next) => {
+    const { id } = req.params
+
+    foldersService.getById(req.app.get('db'), id)
+      .then(folder => {
+        return res
+          .json(folder)
+      })
+  })
+  .delete((req, res, next) => {
+    const { id } = req.params;
+
+    foldersService.deleteFolder(req.app.get('db'), id)
+      .then(deleted => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -24,4 +24,46 @@ notesRouter
       .catch(next)
   })
 
+  .post((req, res, next) => {
+    const { name, modified, folderid, content } = req.body;
+    const newNote = { name, modified, folderid, content }
+
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+      }
+    }
+
+    newNote.name = name;
+    newNote.modified = modified;
+    newNote.folderid = folderid;
+    newNote.content = content;
+
+    notesService.insertNote(
+      req.app.get('db'),
+      newNote
+    )
+      .then(note => {
+        res
+          .status(201)
+          .location(`/api/notes/${note.id}`)
+          .json(serializeNotes(note))
+      })
+      .catch(next)
+  })
+
+notesRouter
+  .route('/:id')
+  .get((req, res, next) => {
+    const { id } = req.params
+
+    notesService.getById(req.app.get('db'), id)
+      .then(note => {
+        return res
+          .json(note)
+      })
+  })
+
 module.exports = notesRouter
